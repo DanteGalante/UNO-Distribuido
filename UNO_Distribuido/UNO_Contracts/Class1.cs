f489c4100617529cd66fae953500caae0b5419d7
Dan <olveradan@hotmail.com>
2020-11-19 17:11:13 -0600

    Ventanas de iniciar sesión y registrar usuario

--- UNO_Distribuido/UNO_Contracts/Class1.cs
+++ UNO_Distribuido/UNO_Contracts/Class1.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UNO_Contracts
 {
-    public class Class1
+    [ServiceContract]
+    public interface ILoginServices
     {
+        [OperationContract]
+        void Login();
     }
 }
