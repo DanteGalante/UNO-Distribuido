fc59cbe163f738bbd3d8172ebfeece23a4650301
Dan <olveradan@hotmail.com>
2020-11-19 17:11:13 -0600

    Ventanas de iniciar sesión y registrar usuario

---
+++ UNO_Distribuido/UNO_DB/UNO_DB_Model.Designer.cs
@@ -0,0 +1,10 @@
+﻿// T4 code generation is enabled for model 'D:\Documentos\0.0 TAREA\5o semestre\Tecnologías para la construcción\Proyecto\Git\UNO-Distribuido\UNO_Distribuido\UNO_DB\UNO_DB_Model.edmx'. 
+// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
+// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
+// is open in the designer.
+
+// If no context and entity classes have been generated, it may be because you created an empty model but
+// have not yet chosen which version of Entity Framework to use. To generate a context class and entity
+// classes for your model, open the model in the designer, right-click on the designer surface, and
+// select 'Update Model from Database...', 'Generate Database from Model...', or 'Add Code Generation
+// Item...'.
