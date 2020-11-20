e54cd556e58403a0558b4900e32133764f5d0b16
Dan <olveradan@hotmail.com>
2020-11-19 19:48:04 -0600

    Creación de más ventanas, actualización del modelo de bd e implementación de servicios

---
+++ UNO_Distribuido/UNO_Server/LoginServices.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using UNO_Contracts;
+using UNO_DB;
+
+namespace UNO_Server
+{
+    public class LoginServices : ILoginServices
+    {
+        public void IsLogged(int idPlayer)
+        {
+            bool result = false;
+            try
+            {
+                using(UNODBEntities db = new UNODBEntities())
+                {
+                    foreach (var j in db.Players)
+                    {
+                        if(j.idPlayer == idPlayer)
+                        {
+                            result = true;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                //WIP
+            }
+            callback.IsLoggedResult(result);
+        }
+
+        public void Login(string username, string password)
+        {
+            bool result = false;
+            try
+            {
+                using(UNODBEntities db = new UNODBEntities())
+                {
+                    foreach(var player in db.Players)
+                    {
+                        if(player.username == username && player.password == password)
+                        {
+                            result = true;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                //WIP
+            }
+            callback.LoginVerification(result);
+        }
+        ILoginServicesCallback callback
+        {
+            get
+            {
+                return OperationContext.Current.GetCallbackChannel<ILoginServicesCallback>();
+            }
+        }
+    }
+}
