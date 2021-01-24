0060ce4d3c0d4b677ae461df3264825d4e316d2e
AlanMorgaMora98 <62389250+AlanMorgaMora98@users.noreply.github.com>
2021-01-23 18:57:53 -0600

    Controles de la ventana

--- UNO_Cliente/UNO_Client/GameMainMenuWindow.xaml.cs
+++ UNO_Cliente/UNO_Client/GameMainMenuWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,5 +23,32 @@ namespace UNO_Client
         {
             InitializeComponent();
         }
+
+        private void Btn_Scorers(object sender, RoutedEventArgs e)
+        {
+            BestScorers bestScorers = new BestScorers();
+            bestScorers.Owner = this;
+            this.Hide();
+            bestScorers.ShowDialog();
+            this.Close();
+        }
+
+        private void Btn_CreateRoom(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Btn_Join(object sender, RoutedEventArgs e)
+        {
+         
+        }
+
+        private void Btn_Back(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            this.Owner.ShowDialog();
+        }
     }
+
+
 }
