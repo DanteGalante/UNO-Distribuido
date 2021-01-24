7e6a10e29a0fc6f59ef10a03d32ca403bc5051c7
AlanMorgaMora98 <62389250+AlanMorgaMora98@users.noreply.github.com>
2021-01-23 21:41:13 -0600

    ventanas faltantes

---
+++ UNO_Cliente/UNO_Client/RecoverPassword.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UNO_Client
+{
+    /// <summary>
+    /// Lógica de interacción para RecoverPassword.xaml
+    /// </summary>
+    public partial class RecoverPassword : Window
+    {
+        public RecoverPassword()
+        {
+            InitializeComponent();
+        }
+        
+        private void Btn_SendMeEmail(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            this.Owner.ShowDialog();
+        }
+
+        private void Btn_Back(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            this.Owner.ShowDialog();
+        }
+    }
+}
---
+++ UNO_Cliente/UNO_Client/Settings.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UNO_Client
+{
+    /// <summary>
+    /// Lógica de interacción para Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Audio_Click(object sender, RoutedEventArgs e)
+        {
+            Audio audio = new Audio();
+            audio.Owner = this;
+            this.Hide();
+            audio.ShowDialog();
+            this.Close();
+        }
+
+        private void Btn_Language_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeLanguage changeLanguage = new ChangeLanguage();
+            changeLanguage.Owner = this;
+            this.Hide();
+            changeLanguage.ShowDialog();
+            this.Close();
+        }
+
+        private void Btn_Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            this.Owner.ShowDialog();
+        }
+    }
+}
