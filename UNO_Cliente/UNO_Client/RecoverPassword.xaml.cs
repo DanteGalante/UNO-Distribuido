09be133f4d0029203658cda70f9689d6c7bc8046
AlanMorgaMora98 <62389250+AlanMorgaMora98@users.noreply.github.com>
2021-02-01 13:12:20 -0600

    Actualizacion

--- UNO_Cliente/UNO_Client/RecoverPassword.xaml.cs
+++ UNO_Cliente/UNO_Client/RecoverPassword.xaml.cs
@@ -1,22 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System;
+using System.ServiceModel;
+using UNO_Client.Proxy;
 
 namespace UNO_Client
+
 {
     /// <summary>
     /// Lógica de interacción para RecoverPassword.xaml
     /// </summary>
+    [CallbackBehavior(UseSynchronizationContext = false)]
     public partial class RecoverPassword : Window
     {
         public RecoverPassword()
@@ -26,8 +19,60 @@ namespace UNO_Client
         
         private void Btn_SendMeEmail(object sender, RoutedEventArgs e)
         {
-            this.Hide();
-            this.Owner.ShowDialog();
+            if (tb_UserName.Text != "" && tb_Email.Text != "")
+            {
+                bool result = false;
+
+                InstanceContext instanceContext = new InstanceContext(this);
+                Proxy.PlayerManagerClient client = new PlayerManagerClient();
+                try
+                {
+                    result = client.SendPassword(tb_UserName.Text, tb_Email.Text);
+
+                    if (result == false)
+                    {
+                        MessageRecoveryPasswordError messageChangeError = new MessageRecoveryPasswordError
+                        {
+                            Owner = this
+                        };
+                        messageChangeError.ShowDialog();
+                    }
+                    else
+                    {
+                        MessageRecoveryPassword messageRecoveryPassword = new MessageRecoveryPassword
+                        {
+                            Owner = this
+                        };
+                        messageRecoveryPassword.ShowDialog();
+                    }
+                }
+                catch (EndpointNotFoundException exception)
+                {
+                    Console.WriteLine("No se pudo realizar la conexión con el servidor \n" + exception);
+                    MessageConnection messageConnectionLost = new MessageConnection
+                    {
+                        Owner = this
+                    };
+                    messageConnectionLost.ShowDialog();
+                }
+                catch (TimeoutException exception)
+                {
+                    Console.WriteLine("No se pudo realizar la conexion con el servidor por tiempo \n" + exception);
+                    MessageConnection messageConnectionLost = new MessageConnection
+                    {
+                        Owner = this
+                    };
+                    messageConnectionLost.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageFields messageFields = new MessageFields
+                {
+                    Owner = this
+                };
+                messageFields.ShowDialog();
+            }
         }
 
         private void Btn_Back(object sender, RoutedEventArgs e)
